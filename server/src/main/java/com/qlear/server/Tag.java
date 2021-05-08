6e748c9b59d32f2c3c13eb31988a4431f382edbe
Lambert Liu <lambert0928@outlook.com>
2021-05-08 12:16:47 -0400

    Tag object

---
+++ server/src/main/java/com/qlear/server/Tag.java
@@ -0,0 +1,26 @@
+package com.tago.restservice;
+
+public class Tag {
+    private final String companyId;
+    private final String name;
+    private final String series;
+    private final double price;
+    private final String description;
+    private final String[][] colourways;
+    private final float[][] sizeChart;
+    private final String materials;
+    private final String instructions;
+
+    public Tag(String companyId, String name, String series, double price, String description,
+                String[][] colourways, float[][] sizeChart, String materials, String instructions) {
+        this.companyId = companyId;
+        this.name = name;
+        this.series = series;
+        this.price = Math.round(price * 100) / (double) 100;
+        this.description = description;
+        this.colourways = colourways;
+        this.sizeChart = sizeChart;
+        this.materials = materials;
+        this.instructions = instructions;
+    }
+}
