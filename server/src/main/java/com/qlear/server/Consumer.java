a6710ad8dd853d2ef61614059edfb7d265369ea8
Evan Qi <evanqi@gmail.com>
2021-05-08 14:41:29 -0400

    Consumer class

---
+++ server/src/main/java/com/qlear/server/Consumer.java
@@ -0,0 +1,25 @@
+package com.qlear.restservice;
+import java.util.*;
+
+public class Consumer {
+    private String name;
+    private Set<String> tags;
+    private Set<String> wishlist;
+    public Consumer() {};
+    public Consumer(String name, Set<String> tags, Set<String> wishlist) {
+        this.name = name;
+        this.tags = tags;
+        this.wishlist = wishlist;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public Set<String> getTags() {
+        return this.tags;
+    }
+    public Set<String> getWishlist() {
+        return this.wishlist;
+    }
+
+}
