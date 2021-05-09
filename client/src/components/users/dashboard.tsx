38e6d25678e595f77f7e5a2397e51c97c3e09765
Lambert Liu <lambert0928@outlook.com>
2021-05-09 02:38:22 -0400

    customer dashboard

--- client/src/components/users/dashboard.tsx
+++ client/src/components/users/dashboard.tsx
@@ -5,20 +5,38 @@ import '../../assets/css/customer-dashboard.scss';
 class Dashboard extends React.Component {
   render() {
     return(
-      <div className="content pt-5">
+      <div className="dashboard-main pt-5">
         <div className="container container-custom">
-          <div className="is-size-4 title has-font-weight-400 pt-6 mb-4">Welcome back, Elliot!</div>
-          <div className="is-flex w-100 is-align-items-center">
-            <div className="is-size-5 title has-font-weight-400 mb-2">Recently Scanned</div>
-            <div className="is-flex-grow-1 px-6">
-              <hr className="w-100"/>
+          <div className="is-size-4 title has-font-weight-400 pt-6 mb-6">Welcome back, Elliot!</div>
+
+          <div className="mb-6">
+            <div className="is-flex is-flex-direction-column-touch w-100 is-align-items-center mb-6">
+              <div className="is-size-5 title has-font-weight-400 mb-2">Recently Scanned</div>
+              <div className="is-flex-grow-1 pl-5">
+                <hr className="w-100"/>
+              </div>
+            </div>
+            <div className="columns mx-0 w-100 is-flex-wrap-wrap">
+              <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
+              <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
+              <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
+              <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
             </div>
           </div>
-          <div className="columns mx-0 w-100 is-flex-wrap-wrap">
-            <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
-            <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
-            <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
-            <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
+
+          <div className="mb-6 pt-6">
+            <div className="is-flex is-flex-direction-column-touch w-100 is-align-items-center mb-6">
+              <div className="is-size-5 title has-font-weight-400 mb-2">My Wishlist</div>
+              <div className="is-flex-grow-1 pl-5">
+                <hr className="w-100"/>
+              </div>
+            </div>
+            <div className="columns mx-0 w-100 is-flex-wrap-wrap">
+              <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
+              <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
+              <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
+              <ProductCard company="Patagonia" product="Women's Better Sweater® Fleece Jacket" image="https://cdn.discordapp.com/attachments/758449650403115059/840388729092440074/image0.jpg"/>
+            </div>
           </div>
         </div>
       </div>
