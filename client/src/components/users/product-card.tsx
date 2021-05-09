bdc1370a29189648c8d73c9ddba91fd16f36acf0
Lambert Liu <lambert0928@outlook.com>
2021-05-09 02:12:26 -0400

    update

---
+++ client/src/components/users/product-card.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import CSS from 'csstype';
+
+interface CardProps {
+  company: string;
+  product: string;
+  image: string;
+}
+
+const ProductCard: React.SFC<CardProps> = (props) => {
+  const customCard: CSS.Properties = {
+    borderRadius: "0",
+    backgroundColor: "#F5F5F5",
+    boxShadow: "none"
+  };
+
+  const productImageContainer: CSS.Properties = {
+    position: "relative",
+    paddingBottom: "85%",
+    height: "0",
+    overflow: "hidden"
+  };
+
+  const productImage: CSS.Properties = {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    objectFit: "cover"
+  };
+
+  return (
+    <div className="column mb-6 is-one-quarter-fullhd is-one-third-desktop is-half-tablet is-full-touch">
+      <div className="card mx-1" style={customCard}>
+        <div className="card-content p-0">
+          <div className="content">
+            <div style={productImageContainer}>
+              <img style={productImage} src={props.image} alt="Product preview"/>
+            </div>
+            <div className="is-flex w-100 is-justify-content-between p-4">
+              <div className="opacity-60 company has-text-theme-green-1 has-font-weight-bold">PATAGONIA</div>
+              <div className="is-flex">
+                <i className="fa fa-plus is-text-theme-green-1"/>
+                <i className="fa fa-heart is-text-red"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
